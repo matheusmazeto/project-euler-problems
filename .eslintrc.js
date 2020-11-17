3c139ba0c20967dbb7e88cc7e39ba1871a6ab440
Matheus Mazeto <mgmazeto@gmail.com>
2020-11-16 21:15:33 -0300

    upgrade and config eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,18 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es2021: true,
   },
   extends: [
     'airbnb-base',
+    'plugin:prettier/recommended',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 12,
     sourceType: 'module',
   },
+  plugins: ['prettier'],
   rules: {
+    'prettier/prettier': 'error',
   },
 };
